ab3beacac2e07bf9180ccb82618d9999c65ba852
kimtorres2005 <kimtorres2005@gmail.com>
2023-10-14 10:09:31 -0500

    team 4 assignment. dynamic header and footer

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -2,6 +2,10 @@ import { getData } from "./productData.mjs";
 import {renderListWithTemplate} from "./utils.mjs";
 
 function productCardTemplate(product) {
+  let discountAmount = product.SuggestedRetailPrice - product.FinalPrice;
+  console.log(discountAmount);
+  let discountPrice = Math.round(discountAmount * 100)/100;
+  
     return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
@@ -11,12 +15,15 @@ function productCardTemplate(product) {
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.NameWithoutBrand}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
+    <p class="product-card__originalprice"><em>$${discountPrice} off!</em></p></a>
   </li>`;
 }             
 
 export default async function productList(selector, category) {
-    const el = document.querySelector(selector);
-    const products = await getData(category);
-    console.log(products);
-    renderListWithTemplate(productCardTemplate, el, products);
+  const el = document.querySelector(selector);
+  const products = await getData(category);
+
+  const filteredTents = products.filter(product => product.Name.includes("Tent")).slice(0, 4);
+
+  renderListWithTemplate(productCardTemplate, el, filteredTents);
 }
